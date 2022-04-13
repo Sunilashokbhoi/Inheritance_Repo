2a4ccc7eff19a1ee30bca24b9d57389cf7c94669
Sunilashokbhoi <sunilbhoi606@gmail.com>
2022-04-13 11:13:42 +0530

    MODIFIED: Hierarchical Inheritance

--- src/com/infogalaxy/Result.java
+++ src/com/infogalaxy/Result.java
@@ -1,31 +1,31 @@
-package com.infogalaxy;
-
-public class Result extends Internal{
-    int total;
-    int perc;
-    void setPerc(){
-        System.out.println("Enter The Percentage :");
-        //  perc=sc.nextInt();
-        total = m1 + m2;
-        perc =total/2;
-    }
-    void getResult(){
-        System.out.println("ID :"+id);
-        System.out.println("Name :"+name);
-        System.out.println("M1 Marks :"+m1);
-        System.out.println("M2 Marks :"+m2);
-        System.out.println("Total Marks"+total);
-        System.out.println("Percentage :"+perc);
-    }
-}
-class main{
-    public static void main(String[] args){
-        com.infogalaxy.Result result = new com.infogalaxy.Result();
-        result.setStudent();
-        result.setMarks();
-        result.setPerc();
-        result.getResult();
-    }
-}
-
-
+//package com.infogalaxy;
+//
+//public class Result extends Internal{
+//    int total;
+//    int perc;
+//    void setPerc(){
+//        System.out.println("Enter The Percentage :");
+//        //  perc=sc.nextInt();
+//        total = m1 + m2;
+//        perc =total/2;
+//    }
+//    void getResult(){
+//        System.out.println("ID :"+id);
+//        System.out.println("Name :"+name);
+//        System.out.println("M1 Marks :"+m1);
+//        System.out.println("M2 Marks :"+m2);
+//        System.out.println("Total Marks"+total);
+//        System.out.println("Percentage :"+perc);
+//    }
+//}
+//class main{
+//    public static void main(String[] args){
+//        com.infogalaxy.Result result = new com.infogalaxy.Result();
+//        result.setStudent();
+//        result.setMarks();
+//        result.setPerc();
+//        result.getResult();
+//    }
+//}
+//
+//
--- src/com/infogalaxy/Student.java
+++ src/com/infogalaxy/Student.java
@@ -7,19 +7,55 @@ public class Student {
     int id ;
     String name ;
     void setStudent(){
-        //  System.out.println("Enter The ");
-        System.out.println("Enter The ID And NAME :");
+        System.out.println("Enter The ID :");
         id = sc.nextInt();
+        System.out.println("Enter The Name:");
         name = sc.next();
     }
-}
+    }
 class Internal extends Student{
-    int m1 ;
-    int m2 ;
-    void setMarks(){
-        System.out.println("Enter Marks M1 And M2:");
-        m1 = sc.nextInt();
-        m2 = sc.nextInt();
+    int im1 ;
+    int im2 ;
+    void setInternal(){
+        System.out.println("Enter The im1 Marks:");
+        im1 = sc.nextInt();
+        System.out.println("Enter The im2 Marks");
+        im2 = sc.nextInt();
+    }
+    void getInternal(){
+        System.out.println("Internal ID:"+id);
+        System.out.println("Internal Name:"+name);
+        System.out.println("im1 :"+im1);
+        System.out.println("im2 :"+im2);
+    }
+}
+class External extends Student{
+    int em1;
+    int em2;
+       void setExternal(){
+           System.out.println("Enter Em1 :");
+           em1 = sc.nextInt();
+           System.out.println("Enter EM2");
+           em2 = sc.nextInt();
+       }
+       void getExternal(){
+           System.out.println("Student External ID:"+id);
+           System.out.println("Student External Name:"+name);
+           System.out.println("Em1:"+em1);
+           System.out.println("Em2:"+em2);
+       }
+}
+class Main{
+    public static void main(String[] args){
+       Internal internal = new Internal();
+       External external = new External();
+       internal.setStudent();
+       internal.setInternal();
+       external.setStudent();
+       external.setExternal();
+       internal.getInternal();
+       external.getExternal();
+
     }
 }
 
